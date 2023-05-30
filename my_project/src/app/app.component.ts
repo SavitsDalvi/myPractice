d35d0bf2a548f557d58b511414e297cc83eeb7e1
savita@88 <sav.dalvi88@gmail.com>
2023-05-30 07:44:14 +0530

    loginform practice

--- my_project/src/app/app.component.ts
+++ my_project/src/app/app.component.ts
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my_project';
+  //propertyName :datatype = value/data;
+  //testcase1
+  firstName : string = 'savita';
+
+  //testcase 2
+  firstName1 = 'savita';
+  firstName2! :string;
+  surname2 :any;
+  surName :any =true;
+  formTitle : string ='Login Form..';
+  studentName='sonali';
+  amount= '6500';
+  isFavCity = false;
+
+  testingInterpolation( x :number, y :number){
+    return x + y;
+  } 
+  }
+
 }
