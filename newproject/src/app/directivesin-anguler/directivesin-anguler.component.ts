7eb3c4e620b07bff9ebc33a0dd5c333fb3b0f8e9
savita@88 <sav.dalvi88@gmail.com>
2023-07-06 01:19:12 +0530

    apicall

---
+++ newproject/src/app/directivesin-anguler/directivesin-anguler.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-directivesin-anguler',
+  templateUrl: './directivesin-anguler.component.html',
+  styleUrls: ['./directivesin-anguler.component.css']
+})
+export class DirectivesinAngulerComponent {
+  show=false;
+  toShowPara!:string;
+  show1=true;
+  toshowpara1:string='savita';
+  Undefinedproperty:any;
+  //ullProperty : null;
+  toggel:boolean=false;
+
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+ 
+
+}
